c48f0356da802bf41a5e145d40c54d5ad3bf5d69
Ashish Lahane <lahaneashish123@gmail.com>
2026-03-17 19:48:40 +0530

    Code Commit

---
+++ src/test/java/com/demo/Hello/word/HelloWordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.Hello.word;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
